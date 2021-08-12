34b45ae80dee82d73b7056feabf0fd0bf3ea5c5a
jc-audet <jcgagnon74@gmail.com>
2021-08-12 18:53:36 -0400

    Some small changes

--- datasets.py
+++ datasets.py
@@ -99,7 +99,7 @@ class Single_Domain_Dataset:
         return loaders_ID, loaders
 
 class Multi_Domain_Dataset:
-    N_STEPS = 501
+    N_STEPS = 5001
     CHECKPOINT_FREQ = 50
     SETUP = None
     PRED_TIME = [None]
