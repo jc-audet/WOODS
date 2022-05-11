ab218a035e049346a4d389192b20025fb5fb78c4
jc-audet <jcgagnon74@gmail.com>
2022-05-10 22:33:02 -0400

    Change hyperparameters for balanced monthly aus elec

--- woods/hyperparams.py
+++ woods/hyperparams.py
@@ -313,7 +313,7 @@ def AusElectricityMonthlyBalanced_train(sample):
             'class_balance': lambda r: True,
             'weight_decay': lambda r: 0.,
             'lr': lambda r: 10**r.uniform(-5, -3),
-            'batch_size': lambda r: int(2**r.uniform(5, 7))
+            'batch_size': lambda r: int(2**r.uniform(5, 6))
         }
     else:
         return {
